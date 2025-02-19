3f480993a184d7732637262fd4bb991cd543816d
vasanthcodes <vasanthsanku2719@gmail.com>
2025-02-19 22:35:19 +0530

    added few changes

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-
-
-const button1= document.querySelector("#confirmGridNumber")
-.addEventListener('click',drawGrid)
-const mainDiv=document.querySelector(".mainDiv")
-function drawGrid(){
-    // const gridNumber = Number(document.querySelector(".inputgrid").value);
-    // gridNumber *= gridNumber;
-    // if(gridNumber<=64){
-    for(let i = 1;i<64;i++){
-    const grid=document.createElement("div");
-    grid.style.backgroundColor="red"
-    mainDiv.appendChild(grid)}
-    // else{
-    //     alert("please enter a valid number")
-    }
-// }
-
+const grid=document.querySelector(".container");
+const reset=document.querySelector("#reset");
+const num=document.querySelector("#confirmGridNumber");
 
