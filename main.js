b40e9529e56c543b07388e9f80a180647fe0944c
vasanthcodes <vasanthsanku2719@gmail.com>
2025-02-20 23:39:49 +0530

    finished grid formation

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
-const grid=document.querySelector(".container");
-const reset=document.querySelector("#reset");
-const num=document.querySelector("#confirmGridNumber");
+const grid = document.querySelector(".container");
+const reset = document.querySelector("#reset");
+const num = document.querySelector("#confirmGridNumber");
+let box
+function newGrid(size) {
+  grid.innerHTML = "";
+  let gridSize = document.querySelector(".input-grid").value;
+  if (size === undefined) {
+    size = 16;
+  }
 
-
-
-function newGrid(size){
-   grid.innerHTML="";
-   let gridSize= document.querySelector(".input-grid").value;
-   if(size===undefined){
-    size=16;
-   }
-
-   size=gridSize;
-   for(let i=1;i<=size**2;i++){
-    const box=document.createElement("div");
+  size = gridSize;
+  for (let i = 1; i <= size ** 2; i++) {
+    box = document.createElement("div");
     grid.appendChild(box);
-    let width=600/size;
-    box.setAttribute("style",`width:${width}px; height: ${width}px; background-color: white`);
-    }
-   }
-num.addEventListener("click",newGrid);
+    let width = 600 / size;
+    box.setAttribute(
+      "style",
+      `width:${width}px; height: ${width}px; background-color: white`
+    );
+  }
+}
+
+function hover() {
+   box.style.backgroundColor="black";
+}
+
+num.addEventListener("click", newGrid);
+grid.addEventListener("mouseover",hover)
