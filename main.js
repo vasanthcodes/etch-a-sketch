cf39ef92ab3b21066a3dac9d3f3a65ccbff81379
vasanthcodes <vasanthsanku2719@gmail.com>
2025-02-20 23:18:33 +0530

    finished grid formation

--- main.js
+++ main.js
@@ -2,3 +2,21 @@ const grid=document.querySelector(".container");
 const reset=document.querySelector("#reset");
 const num=document.querySelector("#confirmGridNumber");
 
+
+
+function newGrid(size){
+   grid.innerHTML="";
+   let gridSize= document.querySelector(".input-grid").value;
+   if(size===undefined){
+    size=16;
+   }
+
+   size=gridSize;
+   for(let i=1;i<=size**2;i++){
+    const box=document.createElement("div");
+    grid.appendChild(box);
+    let width=600/size;
+    box.setAttribute("style",`width:${width}px; height: ${width}px; background-color: white`);
+    }
+   }
+num.addEventListener("click",newGrid);
